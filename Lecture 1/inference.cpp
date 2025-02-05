c836859b2b9f76fb2b24f9a149ffdf4fb946081a
Daheda1 <danni.dahl@gmail.com>
2025-02-05 10:36:55 +0100

    Added to train and started C++ code

---
+++ Lecture 1/inference.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std
+
+int main()
+{
+    cout << "Starting main function" << endl;
+    return 0;
+}
--- Lecture 1/train.py
+++ Lecture 1/train.py
@@ -3,28 +3,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
-
-
-class ImageNet(nn.Module):
-    def __init__(self):
-        super(ImageNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)     
-        self.pool = nn.MaxPool2d(2, 2)        
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(x.size(0), -1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+from modelstructure import ImageNet
     
-
 def train(dataloader, net, criterion, optimizer, epochs=10):
     print("Training")
     for epoch in range(epochs):
@@ -44,12 +24,12 @@ def train(dataloader, net, criterion, optimizer, epochs=10):
                 running_loss = 0.0 
     print("Finished Training")
 
+def model_export():
+    torch_model = ImageNet()
 
 def main():
-    # Define the transform to convert images to tensors
     transform = transforms.ToTensor()
 
-    # Load the CIFAR10 training dataset
     trainset = torchvision.datasets.CIFAR10(
         root='./data', train=True, download=True, transform=transform
     )
