8619788a858747a8e44910460637c231b6feddc9
Jacob Selbo <144399384+jacobselbok@users.noreply.github.com>
2025-02-10 12:01:30 +0100

    still testing pre-commit

--- utils.py
+++ utils.py
@@ -8,6 +8,7 @@ import os
 from datetime import datetime
 import json
 
+
 def device_conf():
     # Device configuration (MPS for Mac, CUDA for other GPUs, CPU fallback)
     if torch.backends.mps.is_available():
@@ -31,26 +32,35 @@ def load_config(config_path):
 
 def data_load(config):
     transform = transforms.ToTensor()
-   # val_transform = transforms.Compose([
+    # val_transform = transforms.Compose([
 
+    # ])
 
-        
-   # ])
+    # Downloading dataset
+    trainset = torchvision.datasets.CIFAR10(
+        root=config['dataset']['data'], train=True, download=True, transform=transform
+    )
 
-    #Downloading dataset
-    trainset = torchvision.datasets.CIFAR10(root=config['dataset']['data'], train=True, download=True, transform=transform)
-    
-    #Splitting data into Training and validation 
+    # Splitting data into Training and validation
     train_size = int(config['train']['train_test_split'] * len(trainset))
     val_size = len(trainset) - train_size
-    train_subset, val_subset = torch.utils.data.random_split(trainset, [train_size, val_size])
+    train_subset, val_subset = torch.utils.data.random_split(
+        trainset, [train_size, val_size]
+    )
 
+    train_loader = torch.utils.data.DataLoader(
+        train_subset,
+        batch_size=config['train']['batch_size'],
+        shuffle=True,
+        num_workers=config['train']['num_workers'],
+    )
 
-    train_loader = torch.utils.data.DataLoader(train_subset, batch_size=config['train']['batch_size'], 
-                                               shuffle=True, num_workers=config['train']['num_workers'])
-
-    val_loader = torch.utils.data.DataLoader(val_subset, batch_size=config['train']['batch_size'], 
-                                             shuffle=False, num_workers=config['train']['num_workers'])
+    val_loader = torch.utils.data.DataLoader(
+        val_subset,
+        batch_size=config['train']['batch_size'],
+        shuffle=False,
+        num_workers=config['train']['num_workers'],
+    )
     return train_loader, val_loader
 
 
@@ -59,17 +69,27 @@ def pick_optimizer(model, config):
         optimizer = torch.optim.Adam(model.parameters(), lr=config['train']['lr'])
     return optimizer
 
+
 def pick_scheduler(optimizer, config):
     if config['train']['scheduler'] == 'steplr':
-        scheduler = StepLR(optimizer, step_size=config['train']['step_size'], gamma=config['train']['gamma'])
+        scheduler = StepLR(
+            optimizer,
+            step_size=config['train']['step_size'],
+            gamma=config['train']['gamma'],
+        )
     return scheduler
+
 
 def begin_wandb():
     wandb_dir = "wandb"
     if not os.path.exists(wandb_dir):
         runs = []
     else:
-        runs = [d for d in os.listdir(wandb_dir) if os.path.isdir(os.path.join(wandb_dir, d))]
+        runs = [
+            d
+            for d in os.listdir(wandb_dir)
+            if os.path.isdir(os.path.join(wandb_dir, d))
+        ]
 
     wandb.login()
     wandb.init(project='MLOps', name=f"Job {len(runs)}")
@@ -88,13 +108,12 @@ def model_export(model, device, config):
         model,
         dummy_input,
         onnx_filename,
-        opset_version=11, 
-        input_names=["input"], 
-        output_names=["output"],  
-        dynamic_axes={  
+        opset_version=11,
+        input_names=["input"],
+        output_names=["output"],
+        dynamic_axes={
             "input": {0: "batch_size"},
             "output": {0: "batch_size"},
-        }
+        },
     )
     print(f"Model & config file saved as: {onnx_filename} & {config_filename}")
-
