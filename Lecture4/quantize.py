fde484ff9a3921e8627968a87ff2a4d4d42450b1
Jacob Selbo <144399384+jacobselbok@users.noreply.github.com>
2025-06-09 05:59:30 +0200

    model versioned quantized original model using dvc

--- Lecture4/quantize.py
+++ Lecture4/quantize.py
@@ -3,8 +3,8 @@ from onnxruntime.quantization import quantize_dynamic, QuantType
 
 
 def quantize_onnx_model(
-    saved_model_path="models/deepspeed_stage_3/model.onnx",
-    output_path="models/deepspeed_stage_3/model_quantized.onnx",
+    saved_model_path="models/model_org/model.onnx",
+    output_path="models/model_org/model_quantized.onnx",
 ):
     print(f"Loading ONNX model from: {saved_model_path}")
     model = onnx.load(saved_model_path)
